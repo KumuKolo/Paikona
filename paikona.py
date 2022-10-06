eb8c5d88b1b97bc54f4d8be6546d0274a1851d4f
koloApps <36248431+kolomon-github@users.noreply.github.com>
2022-10-05 21:14:38 -1000

    Create paikona.py

---
+++ paikona.py
@@ -0,0 +1,26 @@
+
+# He hoomaka API
+
+def pai(x):
+    print(x)
+
+def ano(x):
+    return(type(x))
+
+def loihi(x):
+    return(len(x))
+
+def komo(x):
+    return(input(x))
+
+def koli(x):
+    return(round(x))
+
+def kaula(x):
+    return(str(x))
+
+def helu(x):
+    return(int(x))
+
+def papa(x):
+    return(list(x))
